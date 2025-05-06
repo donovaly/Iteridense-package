7abfa3e5275ef1d34bf30a2b6a4ee1a35faccfc1
Uwe Stöhr <uwestoehr@lyx.de>
2025-05-06 05:36:45 +0300

    LazarusExample: add missing form files

---
+++ IteridenseCLib/LazarusExample/AboutForm.pas
@@ -0,0 +1,53 @@
+unit AboutForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  LCLIntf;
+
+type
+
+  { TAboutFormF }
+
+  TAboutFormF = class(TForm)
+    OKBB: TBitBtn;
+    ReadmeLinkL: TLabel;
+    NameL: TLabel;
+    DescriptionTextST: TStaticText;
+    VersionNumberL: TLabel;
+    UsageTextST: TStaticText;
+    SourceCodeTextL: TLabel;
+    GitHubLinkL: TLabel;
+    procedure GitHubLinkLClick(Sender: TObject);
+    procedure ReadmeLinkLClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  AboutFormF: TAboutFormF;
+
+implementation
+
+{$R *.lfm}
+
+{ TAboutFormF }
+
+procedure TAboutFormF.ReadmeLinkLClick(Sender: TObject);
+begin
+  OpenURL('https://codeberg.org/Soloof/Iteridense/raw/branch/main/Paper/Iteridense-clustering.pdf');
+end;
+
+procedure TAboutFormF.GitHubLinkLClick(Sender: TObject);
+begin
+  OpenURL('https://codeberg.org/donovaly/Iteridense-package/src/branch/main/IteridenseCLib/LazarusExample');
+end;
+
+
+end.
+
